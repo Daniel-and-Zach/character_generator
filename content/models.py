19e2b0e7b24a236a41adc577630f893fbe63c858
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-06-14 18:17:17 -0400

    Started making the bare bones of this project.

--- content/models.py
+++ content/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 class Race(models.Model):
+    name = models.CharField(max_length=255, choices=base_races)
+    size = models.CharField(max_length=255)
+    base_speed = models.IntegerField()
+    description = models.TextField()
+    history = models.TextField()
 
-class RacialTrait(models.Model):
+base_races = ["Elf", "Dwarf", "Gnome", "Half-Elf",
+              "Halfling", "Half-Orc", "Human"]
 
-class RacialFeat(models.Model):
-
-class AbiilityModifier(models.Model):
+# class RacialTrait(models.Model):
+#
+# class RacialFeat(models.Model):
+#
+# class AbiilityModifier(models.Model):
